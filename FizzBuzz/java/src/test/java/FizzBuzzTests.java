f508b2f8701827ea51a67e1096e148b9e85d5db9
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-11-18 22:46:06 +0100

    refactoring step

--- FizzBuzz/java/src/test/java/FizzBuzzTests.java
+++ FizzBuzz/java/src/test/java/FizzBuzzTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTests {
 
     @Test
-    public void fb_when_number_is_not_multiple_of_3_or_5()
+    public void fb_when_number_not_multiple_of_3_or_5_then_return_number()
     {
 	assertEquals(String.valueOf(1), FizzBuzz.fb(1));
     }

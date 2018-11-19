1d47507eaa6df26af3ad230759a09a39d10b49c8
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-11-19 01:27:46 +0100

    2nd Unit Test Case - Testing the behaviour when the number is multiple of 3

--- FizzBuzz/Scala/src/main/scala/FizzBuzz.scala
+++ FizzBuzz/Scala/src/main/scala/FizzBuzz.scala
@@ -2,6 +2,15 @@
 object FizzBuzz {
 
   def fb(n: Int) : String = {
-    return "n"
+    return fb(n, isMultipleOf(n, 3))
+  }
+
+  def fb(n: Int, f: Boolean) : String = f match {
+    case true => return "Fizz"
+    case _ => return n.toString()
+  }
+
+  def isMultipleOf(n: Int, x: Int) : Boolean = {
+    return 0 == n % x
   }
 }
--- FizzBuzz/Scala/src/test/scala/FizzBuzzSpec.scala
+++ FizzBuzz/Scala/src/test/scala/FizzBuzzSpec.scala
@@ -5,6 +5,11 @@ class FizzBuzzSpec extends FlatSpec {
   behavior of "Number not multiple of 3 or 5"
 
   it should "return number" in {
-    assert("n" === FizzBuzz.fb(1))
+    assert("1" === FizzBuzz.fb(1))
+  }
+
+  behavior of "Number multiple of 3"
+  it should "return 'Fizz'" in {
+    assert("Fizz" === FizzBuzz.fb(3))
   }
 }

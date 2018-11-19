840596a80d3dbde874a2b7df4187ec9addb012a6
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-11-20 00:36:28 +0100

    Implementing the behavior of number divisible by 3

--- FizzBuzz/Scala/src/main/scala/FizzBuzz.scala
+++ FizzBuzz/Scala/src/main/scala/FizzBuzz.scala
@@ -1,16 +1,51 @@
 object FizzBuzz {
 
+  private var filters : List[(Int) => String] = Nil
+
   def stringFor(number: Int): Any =
   {
     throwNumberLessThanOneException(number)
-    return number
+    return convert(number)
   }
 
-  private def throwNumberLessThanOneException(number: Int): Any =
+  def addFilter(filter: (Int) => String) =
+  {
+    filters = filter::filters
+  }
+
+  private def throwNumberLessThanOneException(number: Int) =
     (1 > number) match {
       case true =>
         throw new NumberLessThanOneException(number)
       case _ =>
         ;
     }
+
+  private def convert(number : Int): Any =
+  {
+    val stringForNumber = applyFilters(number)
+    stringForNumber.isEmpty() match
+    {
+      case true =>
+        return number
+      case _ =>
+        return stringForNumber
+    }
+  }
+
+  private def applyFilters(number: Int): String =
+  {
+    return applyFilters(number, filters.reverse, "")
+  }
+
+  private def applyFilters(number: Int, filters: List[(Int) => String], stringForNumber: String): String =
+  {
+    filters match
+    {
+      case Nil =>
+        return stringForNumber
+      case _ =>
+        applyFilters(number, filters.tail, stringForNumber.concat(filters.head(number)))
+    }
+  }
 }
--- FizzBuzz/Scala/src/test/scala/FizzBuzzSpec.scala
+++ FizzBuzz/Scala/src/test/scala/FizzBuzzSpec.scala
@@ -2,10 +2,19 @@ import org.scalatest.FlatSpec
 
 class FizzBuzzKadaiSpec extends FlatSpec {
 
+  val Fizz = (number: Int) => (0 == number % 3) match {case true => "Fizz" case _ => ""}
+
+  FizzBuzz.addFilter(Fizz)
+
   behavior of "`stringFor` when number is less than 1"
   it should "throw Exception" in {
     assertThrows[Exception] {
       FizzBuzz.stringFor(0)
     }
+  }
+
+  behavior of "`stringFor` when number is divisible by 3"
+  it should "return 'Fizz'" in {
+    assert("Fizz" === FizzBuzz.stringFor(3))
   }
 }

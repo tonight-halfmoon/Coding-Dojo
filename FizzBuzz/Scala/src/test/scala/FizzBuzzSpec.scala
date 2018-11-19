c318b8fb10cd5df9a3043eba3ac09badd09527db
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-11-20 00:39:00 +0100

    Client can add more filters to return 'FizzBuzz' for a number multiple of 3 and 5

--- FizzBuzz/Scala/src/test/scala/FizzBuzzSpec.scala
+++ FizzBuzz/Scala/src/test/scala/FizzBuzzSpec.scala
@@ -3,8 +3,10 @@ import org.scalatest.FlatSpec
 class FizzBuzzKadaiSpec extends FlatSpec {
 
   val Fizz = (number: Int) => (0 == number % 3) match {case true => "Fizz" case _ => ""}
+  val Buzz = (number: Int) => (0 == number % 5) match {case true => "Buzz" case _ => ""}
 
   FizzBuzz.addFilter(Fizz)
+  FizzBuzz.addFilter(Buzz)
 
   behavior of "`stringFor` when number is less than 1"
   it should "throw Exception" in {
@@ -16,5 +18,10 @@ class FizzBuzzKadaiSpec extends FlatSpec {
   behavior of "`stringFor` when number is divisible by 3"
   it should "return 'Fizz'" in {
     assert("Fizz" === FizzBuzz.stringFor(3))
+  }
+
+  behavior of "`stringFor` when number is divisible by 3 and 5"
+  it should "return 'FizzBuzz'" in {
+    assert("FizzBuzz" === FizzBuzz.stringFor(15))
   }
 }

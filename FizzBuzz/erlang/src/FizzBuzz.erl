050a00aa304dd84c323231b699d4247e7d2d35ec
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-12-10 16:59:08 +0100

    Refactoring: Extracted Module -> Separtion of Concern

--- FizzBuzz/erlang/src/FizzBuzz.erl
+++ FizzBuzz/erlang/src/FizzBuzz.erl
@@ -28,22 +28,7 @@ process(Input) when is_integer(Input) andalso Input > 0 ->
     filter(Input).
 
 filter(Input) ->
-    Result = applyFilters(Input),
-    case length(Result) of
-	0 ->
-	    integer_to_list(Input);
-	_ ->
-	    Result
-    end.
-
-applyFilters(Input) ->
-    Filters =  'FiltersProvider':filters(),
-    applyFilters(Input, Filters, "").
-
-applyFilters(_Input, [], Result) ->
-	Result;
-applyFilters(Input, [NextFilter|Filters], Result) ->
-	applyFilters(Input, Filters, lists:concat([NextFilter(Input), Result])).
+    'FilterJob':filter(Input).
 
 print(Value) ->
     Printers = 'PrintersProvider':printers(),

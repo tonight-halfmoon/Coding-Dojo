f678fbd75ef6aff6570d06f76b395375c3de32cb
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-12-16 15:42:39 +0000

    Refactoring: made as final

--- FizzBuzz/java/src/main/java/IntegerLessThanOneException.java
+++ FizzBuzz/java/src/main/java/IntegerLessThanOneException.java
@@ -1,4 +1,4 @@
-class IntegerLessThanOneException extends RuntimeException {
+final class IntegerLessThanOneException extends RuntimeException {
     public IntegerLessThanOneException(int input) {
 	super("Expected input to be greater than 0, but found ".concat(String.valueOf(input)));
     }

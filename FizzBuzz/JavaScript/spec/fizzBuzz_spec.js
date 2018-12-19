68451b0532a8a5c7c784a5588513f776b44ae0bd
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-12-19 22:32:06 +0100

    Refactoring

--- FizzBuzz/JavaScript/spec/fizzBuzz_spec.js
+++ FizzBuzz/JavaScript/spec/fizzBuzz_spec.js
@@ -1,17 +1,17 @@
 describe('stringFor', function() {
     it ('`stringFor` when input is processed, then return input as String', function() {
-	var input = 1;
-	var expected = '1';
+	const input = 1;
+	const expected = '1';
 
-	var actual = fizzBuzz.stringFor(input);
+	const actual = fizzBuzz.stringFor(input);
 
 	expect(actual).toEqual(expected);
     });
 
     it('´stringFor´ when input is less than one, then throw IntegerLessThanOneException', function() {
-	var input = 0;
+	const input = 0;
 
-	var actualFun = function(){ fizzBuzz.stringFor(input)};
+	const actualFun = function(){ fizzBuzz.stringFor(input)};
 
 	expect(actualFun).toThrow();
     });

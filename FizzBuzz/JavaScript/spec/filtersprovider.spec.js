63f67e9e2f70c3bd2f4fb718fce4d54ecb57297e
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-12-21 21:34:13 +0100

    Reformatted

---
+++ FizzBuzz/JavaScript/spec/filtersprovider.spec.js
@@ -0,0 +1,27 @@
+const filtersProvider = require('../src/index').filtersProvider
+
+describe("FiltersProvider", function() {
+  it("´add´ always increase number of filters by one", function() {
+    const input = () => "Filter"
+    const expected = 1
+
+    const actual = filtersProvider.add(input)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it("´removeAll´ always remove all available filters", function() {
+    filtersProvider.removeAll()
+    const inputFilter1 = () => "filter1"
+    const inputFilter2 = () => "filter2"
+    filtersProvider.add(inputFilter1)
+    const currentSize = filtersProvider.add(inputFilter2)
+    expect(currentSize).toEqual(2)
+    const expected = 0
+
+    const actual = filtersProvider.removeAll()
+
+    expect(actual).toEqual(expected)
+  })
+
+})

2d973cdebab6d4343e2d371c7d56274937711a0b
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-12-21 06:12:32 +0100

    Separated Unit Test Packs

--- FizzBuzz/JavaScript/spec/fizzBuzz_spec.js
+++ FizzBuzz/JavaScript/spec/fizzBuzz_spec.js
@@ -24,29 +24,6 @@ describe('stringFor', function() {
 
 	expect(actual).toEqual(expected);
     });
-
-    it('´addFilter´ always increase number of filters by one', function() {
-	const input = () => "Filter";
-	const expected = 1;
-
-	const actual = filtersProvider.add(input);
-
-	expect(actual).toEqual(expected);
-    });
-
-    it('´removeAllFilters´ always remove all available filters', function() {
-	filtersProvider.removeAll();
-	const inputFilter1 = () => "filter1";
-	const inputFilter2 = () => "filter2";
-	filtersProvider.add(inputFilter1);
-	const currentSize = filtersProvider.add(inputFilter2);
-	expect(currentSize).toEqual(2);
-	const expected = 0;
-
-	const actual = filtersProvider.removeAll();
-
-	expect(actual).toEqual(expected);
-    });
 });
 
 const FizzBuzz = require('../index.js').fizzbuzz;

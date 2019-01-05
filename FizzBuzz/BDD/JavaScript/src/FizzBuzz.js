07483c4a76e9371f2bcc7636b7c510462aa30bf0
Ahmad Elghafari <ahmadelghafari@gmail.com>
2019-01-05 13:39:17 +0000

    Refactoring: Extract Method

--- FizzBuzz/BDD/JavaScript/src/FizzBuzz.js
+++ FizzBuzz/BDD/JavaScript/src/FizzBuzz.js
@@ -3,13 +3,21 @@ const buzzFilter = require('./buzzFilter').buzzFilter
 
 class FizzBuzz {
     stringFor(input) {
-	var result = fizzFilter(input)
-	result += buzzFilter(input)
+	const result = this.filter(input)
 	if (result === '')
 	{
 	    return input.toString()
 	}
 	return result
+    }
+
+    filter(input) {
+	const filters = [fizzFilter, buzzFilter]
+	return this.applyFilters(filters, input)
+    }
+
+    applyFilters(filters, input) {
+	return filters.map(filter => filter(input)).join('')
     }
 }
 
